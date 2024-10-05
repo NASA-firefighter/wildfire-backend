e74e21802efeaf5d19ba3554423bd6606d0b42fb
Dayeon Yoo <udayeon@naver.com>
2024-10-05 15:50:36 -0600

    Feature/add-wildfire-data (#1)
    * 📦️ Add csvtojson
    
    * ✨ Add real wildfire data

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
 
   // Enable CORS
   app.enableCors({
-    origin: 'http://localhost:3000', // Add your front-end's origin here
+    origin: 'http://localhost:3000',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
